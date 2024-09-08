a11d41af69a6286d1349a8623926ac1a758020a4
Welpe1 <318575421@qq.com>
2024-09-09 01:02:36 +0800

    2024.9.9

--- freertos/duo_shell/fip-build.sh
+++ freertos/duo_shell/fip-build.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+Board=256  
+  
+if [ "$Board" == "64" ]; then  
+    Board=soc_cv1800b_milkv_duo_sd
+    Config=cv1800b_milkv_duo_sd
+    ssh_ip=192.168.42.1  
+elif [ "$Board" == "256" ]; then  
+    Board=soc_cv1812cp_milkv_duo256m_sd  
+    Config=cv1812cp_milkv_duo256m_sd
+    ssh_ip=192.168.42.2  
+fi  
 clear
-cd ./install/soc_cv1800b_milkv_duo_sd
+cd ./install/${Board}
 if [ -f fip.bin ]; then  
 	rm ./fip.bin  
 	sleep 2
@@ -19,24 +30,23 @@ cd ../../../../
 
 source device/milkv-duo-sd/boardconfig.sh
 source build/milkvsetup.sh
-defconfig cv1800b_milkv_duo_sd
+defconfig ${Config}
+#clean_rtos
 #clean_uboot
 build_uboot
 
 
-cd ./install/soc_cv1800b_milkv_duo_sd
+cd ./install/${Board}
 if [ -f fip.bin ]; then  
 	ls -l fip.bin  
-	sleep 2
 	echo "need transfer fip.bin?  Y(y)/N(n)"
 	read answer
 
 	# 将用户的输入转换为小写以便比较  
 	answer="${answer,,}"  
 
-	if [[ "$answer" =~ ^(y)$ ]]; then  
-    
-    	scp fip.bin root@192.168.42.1:/root
-    fi  
+	if [[ "$answer" != "n" ]]; then  
+    scp fip.bin root@${ssh_ip}:/root  
+	fi
 fi
 	
